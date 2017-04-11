29dfb7aee7f35f1db4a2bd66018c44e8f8fa5580
Eric Chou <eric@frm.fm>
2017-04-11 10:29:40 +0800

    initial version

---
+++ Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace TeslaQR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtPressfit = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtGear = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPinion = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.picHidBarcode = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picHidBarcode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtPressfit
+            // 
+            this.txtPressfit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPressfit.Location = new System.Drawing.Point(142, 33);
+            this.txtPressfit.Name = "txtPressfit";
+            this.txtPressfit.Size = new System.Drawing.Size(353, 29);
+            this.txtPressfit.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "總成 QR Code";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "餅 QR Code";
+            // 
+            // txtGear
+            // 
+            this.txtGear.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGear.Location = new System.Drawing.Point(142, 196);
+            this.txtGear.Name = "txtGear";
+            this.txtGear.Size = new System.Drawing.Size(353, 29);
+            this.txtGear.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "軸 QR Code";
+            // 
+            // txtPinion
+            // 
+            this.txtPinion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPinion.Location = new System.Drawing.Point(142, 114);
+            this.txtPinion.Name = "txtPinion";
+            this.txtPinion.Size = new System.Drawing.Size(353, 29);
+            this.txtPinion.TabIndex = 2;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnSave.BackgroundImage")));
+            this.btnSave.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(197, 255);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(243, 347);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(512, 18);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(70, 66);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(512, 181);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(70, 66);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox3.Location = new System.Drawing.Point(512, 99);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(70, 66);
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnClear.BackgroundImage")));
+            this.btnClear.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(615, 80);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(99, 90);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // picHidBarcode
+            // 
+            this.picHidBarcode.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picHidBarcode.BackgroundImage")));
+            this.picHidBarcode.Location = new System.Drawing.Point(140, 372);
+            this.picHidBarcode.Name = "picHidBarcode";
+            this.picHidBarcode.Size = new System.Drawing.Size(356, 159);
+            this.picHidBarcode.TabIndex = 11;
+            this.picHidBarcode.TabStop = false;
+            this.picHidBarcode.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 623);
+            this.Controls.Add(this.picHidBarcode);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPinion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtGear);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPressfit);
+            this.Name = "Form1";
+            this.Text = "Tesa QR Code";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picHidBarcode)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPressfit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtGear;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPinion;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.PictureBox picHidBarcode;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HidLibrary;
+using XmlRpc;
+
+namespace TeslaQR
+{
+    public partial class Form1 : Form
+    {
+        private const string PRESSFIT = "P1104383";
+        private const string GEAR = "P1104385";
+        private const string PINION = "P1104384";
+        private static readonly  string[] QUALIFIED_SCANNER_ID = { "0x0907", "0x0927" };
+        private static string odooUrl = "http://192.168.10.143:8069/xmlrpc/2", db = "odoo-batom", pass = "batom", user = "admin";
+        private HidDevice _hidDevice = null;
+        public delegate void ReadHandlerDelegate(HidReport report);
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetHidDevice();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearFields();
+        }
+
+        private void ReadProcess(HidReport report)
+        {
+            BeginInvoke(new ReadHandlerDelegate(ReadHandler), new object[] { report });
+        }
+
+        private void ReadHandler(HidReport report)
+        {
+            int len = Convert.ToInt32(report.Data[0]);
+            string barcode = "";
+            for (int i = 4; i < 4 + len; i++)
+            {
+                barcode += Convert.ToChar(report.Data[i]);
+            }
+            
+            if (barcode == "Save")
+                Save();
+            else
+            {
+                var prefix = barcode.Substring(0, 8);
+                if (prefix == PRESSFIT)
+                    txtPressfit.Text = barcode;
+                else if (prefix == GEAR)
+                    txtGear.Text = barcode;
+                else if (prefix == PINION)
+                    txtPinion.Text = barcode;
+            }
+
+            _hidDevice.ReadReport(ReadProcess);
+        }
+
+        private static void WriteResult(bool success)
+        {
+            MessageBox.Show("Write result: " + success);
+        }
+
+        private void GetHidDevice()
+        {
+            HidDevice[] deviceList = HidDevices.Enumerate(0x0c2e).ToArray();
+            if (deviceList.Length <=0)
+            {
+                MessageBox.Show("找不到 Honeywell 掃描槍", "錯誤");
+            }
+            else
+            {
+                foreach (HidDevice hidDevice in deviceList)
+                {
+                    if (hidDevice.Description == "POS HID Barcode scanner" && QUALIFIED_SCANNER_ID.Contains(hidDevice.Attributes.ProductHexId))
+                    {
+                        _hidDevice = hidDevice;
+                        _hidDevice.OpenDevice();
+                        _hidDevice.MonitorDeviceEvents = true;
+                        _hidDevice.ReadReport(ReadProcess);
+                        break;
+                    }
+                }
+                if (_hidDevice == null)
+                {
+                    picHidBarcode.Visible = true;
+                    btnSave.Visible = false;
+                    MessageBox.Show("請掃描螢幕下方條碼將掃描槍設定為 HID 模式後重新啟動 TeslaQR", "錯誤");
+                }
+            }
+        }
+
+        private void Save() {
+            string pressfit_qr = "";
+            string gear_qr = "";
+            string pinion_qr = "";
+            if (txtPressfit.Text != null)
+                pressfit_qr = txtPressfit.Text.Trim();
+            if (txtGear.Text != null)
+                gear_qr = txtGear.Text.Trim();
+            if (txtPinion.Text != null)
+                pinion_qr = txtPinion.Text.Trim();
+            if (pressfit_qr == "" && gear_qr == "" && pinion_qr == "")
+            {
+                MessageBox.Show("請掃描 QR Code", "錯誤");
+            }
+            else
+            {
+                try
+                {
+                    Cursor.Current = Cursors.WaitCursor;
+                    XmlRpcClient client = new XmlRpcClient();
+                    client.Url = odooUrl;
+                    client.Path = "common";           
+
+                    // LOGIN
+
+                    XmlRpcRequest requestLogin = new XmlRpcRequest("authenticate");
+                    requestLogin.AddParams(db, user, pass, XmlRpcParameter.EmptyStruct());
+
+                    XmlRpcResponse responseLogin = client.Execute(requestLogin);
+
+                    if (responseLogin.IsFault())
+                    {
+                        Cursor.Current = Cursors.Default;
+                        MessageBox.Show("無法連線到資料庫，請通知 IT 人員", "錯誤");
+                    }
+                    else
+                    {
+                        client.Path = "object";
+
+                        List<object> domain = new List<object>();
+                        if (pressfit_qr != "")
+                            domain.Add(XmlRpcParameter.AsArray("pressfit_qr", "=", pressfit_qr));
+                        if (gear_qr != "")
+                            domain.Add(XmlRpcParameter.AsArray("gear_qr", "=", gear_qr));
+                        if (pinion_qr != "")
+                            domain.Add(XmlRpcParameter.AsArray("pinion_qr", "=", pinion_qr));
+                        
+                        if (domain.Count >= 2)
+                        {
+                            if (domain.Count == 3)
+                                domain.Insert(0, "|");
+                            domain.Insert(0, "|");
+                        }
+                        XmlRpcRequest requestSearch = new XmlRpcRequest("execute_kw");
+                        requestSearch.AddParams(db, responseLogin.GetInt(), pass, "batom.tesla.qrcode", "search_read", 
+                            XmlRpcParameter.AsArray(
+                                domain
+                            ),
+                            XmlRpcParameter.AsStruct(
+                                XmlRpcParameter.AsMember("fields", XmlRpcParameter.AsArray("pressfit_qr", "gear_qr", "pinion_qr"))
+                                // ,XmlRpcParameter.AsMember("limit", 2)
+                            )
+                        );                      
+
+                        XmlRpcResponse responseSearch = client.Execute(requestSearch);
+
+                        if (responseSearch.IsFault())
+                        {
+                            MessageBox.Show(responseSearch.GetFaultString(), "錯誤");
+                        }
+                        else if (!responseSearch.IsArray())
+                        {
+                            Cursor.Current = Cursors.Default;
+                            MessageBox.Show("查詢資料庫異常，請通知 IT 人員", "錯誤");
+                        }
+                        else
+                        {
+                            List<Object> valueList = responseSearch.GetArray();
+                            if (valueList.Count == 0)
+                            {
+                                Dictionary<string, object> values = new Dictionary<string, object>();
+                                if (pressfit_qr != "")
+                                    values["pressfit_qr"] = pressfit_qr;
+                                if (gear_qr != "")
+                                    values["gear_qr"] = gear_qr;
+                                if (pinion_qr != "")
+                                    values["pinion_qr"] = pinion_qr;
+                                XmlRpcRequest requestCreate = new XmlRpcRequest("execute_kw");
+                                requestCreate.AddParams(db, responseLogin.GetInt(), pass, "batom.tesla.qrcode", "create", 
+                                    XmlRpcParameter.AsArray(values)
+                                );                      
+
+                                XmlRpcResponse responseCreate = client.Execute(requestCreate);
+                                if (responseCreate.IsFault())
+                                {
+                                    MessageBox.Show(responseCreate.GetFaultString(), "錯誤");
+                                }
+                                else
+                                {
+                                    AutoClosingMessageBox.Show("已儲存", "完成", 1000);
+                                    ClearFields();
+                                }
+                            }
+                            else
+                            {
+                                string db_pressfit_qr = "";
+                                string db_gear_qr = "";
+                                string db_pinion_qr = "";
+                                int id = -1;
+                                foreach (Dictionary<string, object> valueDictionary in valueList)
+                                {
+                                    foreach (KeyValuePair<string, object> kvp in valueDictionary)
+                                    {
+                                        if (kvp.Key == "id")
+                                            id = (int)kvp.Value;
+                                        else if (kvp.Key == "pressfit_qr" && kvp.Value is string)
+                                            db_pressfit_qr = (string)kvp.Value;
+                                        else if (kvp.Key == "gear_qr" && kvp.Value is string)
+                                            db_gear_qr = (string)kvp.Value;
+                                        else if (kvp.Key == "pinion_qr" && kvp.Value is string)
+                                            db_pinion_qr = (string)kvp.Value;
+                                    }
+                                }
+                                
+                                if ((pressfit_qr == "" || pressfit_qr == db_pressfit_qr) &&
+                                    (gear_qr == "" || gear_qr == db_gear_qr) && 
+                                    (pinion_qr == "" || pinion_qr == db_pinion_qr))
+                                {
+                                    Cursor.Current = Cursors.Default;
+                                    MessageBox.Show("QR code 組合已存在", "錯誤");
+                                }
+                                else if (ValueConflict(pressfit_qr, db_pressfit_qr) ||
+                                    ValueConflict(gear_qr, db_gear_qr) ||
+                                    ValueConflict(pinion_qr, db_pinion_qr))
+                                {
+                                    Cursor.Current = Cursors.Default;
+                                    MessageBox.Show("與資料庫中下列 QR code 組合衝突，無法儲存：\n\n" +
+                                        "總成：" + db_pressfit_qr + "\n" +
+                                        "軸：　" + db_pinion_qr + "\n" +
+                                        "餅：　" + db_gear_qr,
+                                        "錯誤"
+                                    );
+                                }
+                                else
+                                {
+                                    Dictionary<string, object> values = new Dictionary<string, object>();
+                                    if (pressfit_qr != "")
+                                        values["pressfit_qr"] = pressfit_qr;
+                                    if (gear_qr != "")
+                                        values["gear_qr"] = gear_qr;
+                                    if (pinion_qr != "")
+                                        values["pinion_qr"] = pinion_qr;
+                                    XmlRpcRequest requestWrite = new XmlRpcRequest("execute_kw");
+                                    requestWrite.AddParams(db, responseLogin.GetInt(), pass, "batom.tesla.qrcode", "write",
+                                        XmlRpcParameter.AsArray(XmlRpcParameter.AsArray(id), values)
+                                    );                      
+
+                                    XmlRpcResponse responseWrite = client.Execute(requestWrite);
+
+                                    if (responseWrite.IsFault())
+                                    {
+                                        MessageBox.Show(responseWrite.GetFaultString(), "錯誤");
+                                    }
+                                    else
+                                    {
+                                        AutoClosingMessageBox.Show("已儲存", "完成", 1000);
+                                        ClearFields();
+                                    }
+                                }
+                            }
+                        }
+                        Cursor.Current = Cursors.Default;
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("無法儲存，請通知IT人員", "錯誤");
+                }
+            }
+        }
+        
+        private bool ValueConflict(string value, string dbValue)
+        {
+            return (value != dbValue && value != "" && dbValue != "");
+        }
+        
+        private void ClearFields()
+        {
+            txtPressfit.Clear();
+            txtGear.Clear();
+            txtPinion.Clear();
+        }
+
+        private class AutoClosingMessageBox
+        {
+            System.Threading.Timer _timeoutTimer;
+            string _caption;
+            AutoClosingMessageBox(string text, string caption, int timeout)
+            {
+                _caption = caption;
+                _timeoutTimer = new System.Threading.Timer(OnTimerElapsed,
+                    null, timeout, System.Threading.Timeout.Infinite);
+                using (_timeoutTimer)
+                    MessageBox.Show(text, caption);
+            }
+            public static void Show(string text, string caption, int timeout)
+            {
+                new AutoClosingMessageBox(text, caption, timeout);
+            }
+            void OnTimerElapsed(object state)
+            {
+                IntPtr mbWnd = FindWindow("#32770", _caption); // lpClassName is #32770 for MessageBox
+                if (mbWnd != IntPtr.Zero)
+                    SendMessage(mbWnd, WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
+                _timeoutTimer.Dispose();
+            }
+            const int WM_CLOSE = 0x0010;
+            [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+            static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+            [System.Runtime.InteropServices.DllImport("user32.dll", CharSet = System.Runtime.InteropServices.CharSet.Auto)]
+            static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
+        }
+    }
+}
